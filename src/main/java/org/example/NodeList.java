c17371dbc4b4ec747ec4aee18293eb0ebc0fa593
Jeremy M <1433767@claurendeau.qc.ca>
2021-09-03 16:18:57 -0400

    Added variables and constructor of NodeList

---
+++ src/main/java/org/example/NodeList.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class NodeList {
+    public Node firstNode;
+    public Node lastNode;
+    private int nbNode;
+
+    public NodeList(){
+        firstNode = null;
+        lastNode = null;
+        nbNode = 0;
+    }
+
+    public void print(int idx){
+
+    }
+
+    public void addNode(Object object){
+
+    }
+
+    public int getSize(){
+        return nbNode;
+    }
+}
