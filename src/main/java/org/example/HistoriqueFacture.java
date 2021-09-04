01b0bd2005e2d0ff102ac8bdffc540a1afbfd590
Jeremy Mailhot <jay_64@outlook.com>
2021-09-03 21:33:44 -0400

    Implemented HistoriqueFacture class

--- src/main/java/org/example/HistoriqueFacture.java
+++ src/main/java/org/example/HistoriqueFacture.java
@@ -1,4 +1,39 @@
 package org.example;
 
 public class HistoriqueFacture {
+    public Node<Facture> firstNode;
+    public Node<Facture> lastNode;
+    private int nbNode;
+
+    public HistoriqueFacture() {
+        firstNode = null;
+        lastNode = null;
+        nbNode = 0;
+    }
+
+    public void ajouterFacture(Facture facture){
+        Node<Facture> newNode = new Node(facture);
+        Node<Facture> tmp;
+
+        if (nbNode > 0){
+            tmp = lastNode;
+            tmp.next = newNode;
+            lastNode = newNode;
+        } else {
+            firstNode = lastNode = newNode;
+        }
+        nbNode++;
+    }
+
+    public Facture[] getFactures() {
+        Facture[] factures = new Facture[nbNode];
+        Node<Facture> tmp;
+
+        tmp = firstNode;
+        for (int i = 0; i < nbNode; i++) {
+            factures[i] = tmp.object;
+            tmp = tmp.next;
+        }
+        return factures;
+    }
 }
